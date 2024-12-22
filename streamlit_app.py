4dc2082aa5ec08b4948f39561c9a35f95214abc1
Amritansh Gupta <b521011@iiit-bh.ac.in>
2024-12-22 14:34:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import re
 import fitz
 import nltk
 nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
 from sqlalchemy.ext.declarative import declarative_base
 import streamlit as st
